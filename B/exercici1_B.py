68e2e4fd3fa86ae5c920711b46340b2b65eba9f7
csanchez20 <csanchez20@jaumebalmes.net>
2023-02-01 20:21:18 +0100

    Practica_10_B amb comentaris

--- B/exercici1_B.py
+++ B/exercici1_B.py
@@ -1,5 +1,8 @@
+# Es veu el import del datatime, la funcio i el calcul del la edat y l'any
 def anyNaixement(edat):
     import datetime
     any = datetime.datetime.now().year
     calculAny = any - edat
     return calculAny
+
+
